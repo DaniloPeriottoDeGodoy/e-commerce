e6d49bf4dc29d8f8adf36ed24112c863f61625d0
Danilo Periotto de Godoy <37711331+DaniloPeriottoDeGodoy@users.noreply.github.com>
2022-10-14 20:10:31 -0300

    Add files via upload

---
+++ e-commerce/src/Api/Controllers/ProdutosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProdutosController : ControllerBase
+	{
+		[HttpPut("{idDoProduto}/Promocao/{idDaPromocao}")]
+		public void VincularPromocaoAoProduto(int idDaPromocao, int idDoProduto)
+		{
+		}
+	}
+}
