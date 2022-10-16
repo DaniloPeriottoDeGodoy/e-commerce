13f281d99fbf9e6919ea562376a2694a5ec93119
Danilo Periotto de Godoy <daniloperiottodegodoy@gmail.com>
2022-10-15 22:59:59 -0300

    Alteração para fazer o endPoint Produto usar somente a ApplicationService e não mais os serviços do domínio.

--- src/Api/Controllers/ProdutosController.cs
+++ src/Api/Controllers/ProdutosController.cs
@@ -8,15 +8,11 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class ProdutosController : ControllerBase
-    {
-        private readonly PromocaoService _promocaoService;
-        private readonly ProdutoService _produtoService;
+    {        
         private readonly IProdutoApplicationService _produtoApplicationService;
 
-        public ProdutosController(PromocaoService promocaoService, ProdutoService produtoService, IProdutoApplicationService produtoApplicationService)
-        {
-            _promocaoService = promocaoService;
-            _produtoService = produtoService;
+        public ProdutosController(IProdutoApplicationService produtoApplicationService)
+        {            
             _produtoApplicationService = produtoApplicationService;
         }
 
@@ -25,17 +21,7 @@ namespace Api.Controllers
         {
             try
             {
-
-                _produtoApplicationService.VincularPromocaoAoProduto(idDaPromocao, idDoProduto);
-                //var produto = _produtoService.ObterProdutoPorId(idDoProduto);
-                //if (produto == null)
-                //    throw new Exception("Código de produto não encontrado.");
-
-                //var promocao = _promocaoService.ObterPromocaoPorId(idDaPromocao);
-                //if (promocao == null)
-                //    throw new Exception("Código de promoção não encontrado.");
-
-                //_produtoService.VincularProdutoNaPromocao(idDaPromocao, idDoProduto);
+                _produtoApplicationService.VincularPromocaoAoProduto(idDaPromocao, idDoProduto);                
                 
             }
             catch (Exception e)
